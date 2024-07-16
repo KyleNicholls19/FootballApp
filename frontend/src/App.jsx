f007482991cb9c9fce613d43c8c4ac8e008e4bf6
KyleNicholls19 <kyle.nicholls190@gmail.com>
2024-07-15 21:23:38 -0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { useState,useEffect } from 'react'
 
 function App() {
 
-  const [table,setTable] = useState();
+  const [table,setTable] = useState([]);
 
   useEffect(() => {
     getTable()
@@ -14,12 +14,15 @@ function App() {
     const data = await response.json()
 
     for (const key in data) {
-      for (const item in data[key]){
-        console.log(data[key][item])
-      }
+      const newData = {Pos: Object.values(data[key])[0], Team: data[key].Team, Pl: data[key].Pl,W: data[key].W,D: data[key].D,L: data[key].L,F: data[key].F,
+        A: data[key].A,GD: data[key].GD,Pts: data[key].Pts}
+
+      setTable(table.push(newData))
+      
     }
 
-    //console.log(data)
+
+    console.log(table)
   }
 
   return (
