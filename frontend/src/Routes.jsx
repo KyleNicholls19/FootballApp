1430fd05ffcec6e277cec36216ccb5809632bafa
KyleNicholls19 <kyle.nicholls190@gmail.com>
2024-07-20 19:33:41 -0600

    comments and removed import

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -6,11 +6,13 @@ import {createBrowserRouter} from 'react-router-dom'
 
 function Routes() {
     return createBrowserRouter([
+        // Home path whenever the site is first accessed
         {
             path: '/',
             element: <Home/>,
             errorElement: <PageNotFound/>,
         },
+        // table paths gathering the league name to find specific league
         {
             path: '/table/:leagueName',
             element: <GetTable/>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import {RouterProvider} from 'react-router-dom'
 import Routes from './Routes.jsx'
 
 
