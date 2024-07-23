f727ffc404c5652d1b9d58c8e95d9d9c785b0160
KyleNicholls19 <kyle.nicholls190@gmail.com>
2024-07-22 20:31:15 -0600

    Update NavBar.jsx

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -1,4 +1,4 @@
-import { Anchor, Center, Container, Group, Menu, ActionIcon,Drawer,Popover ,NavLink,Flex,Box,Button } from '@mantine/core';
+import { Anchor, Center, Container, Group, Menu, ActionIcon,Drawer,Popover ,NavLink,Flex,Box,Button, ScrollArea } from '@mantine/core';
 import { Link } from 'react-router-dom';
 import {names,PremierLeagueTeams} from './NameConversion.json'
 import {IconHome} from '@tabler/icons-react'
@@ -12,6 +12,13 @@ General use navbar to be displayed on multiple parts of the site
 */
     return (
       <>
+
+      
+      <Flex direction='row' style={{width: '100%'}}justify='center' align='flex-start'>
+        <ActionIcon variant='hover' component={Link} to='/'>
+        <IconHome></IconHome>
+        </ActionIcon> 
+
         <Menu openDelay={100} closeDelay={400}>
         <Menu.Target>
         <Anchor target="_blank" underline="hover" className='nav-items'>Fixtures</Anchor>
@@ -23,45 +30,21 @@ General use navbar to be displayed on multiple parts of the site
             <Menu.Target>
               <div>Premier League</div>
             </Menu.Target>
+            
             <Menu.Dropdown>
               <Menu.Label>Teams</Menu.Label>
+              <ScrollArea h={400}>
               <Menu.Item component={Link} to={`/fixtures/league/premier-league`}>All Premier League Fixtures</Menu.Item>
               {PremierLeagueTeams.map((item) => (
                 <Menu.Item component={Link} to={`/fixtures/team/${item.url}`} key={item.url}>{item.display}</Menu.Item>
               ))}
+              </ScrollArea>
             </Menu.Dropdown>
           </Menu>
-        </Menu.Item>
-      </Menu.Dropdown>
-    </Menu>
-      
-      <Flex direction='row' style={{width: '100%'}}justify='center' align='flex-start'>
-        <ActionIcon variant='hover' component={Link} to='/'>
-        <IconHome></IconHome>
-        </ActionIcon> 
-        <Box width={240}>  
-          <NavLink
-            href="#required-for-focus"
-            label="Fixtures"
-            childrenOffset={3}
-            className='NavLink'>         
-
-
-          <NavLink
-          href="#required-for-focus"
-          label="Premier League"
-          childrenOffset={28}
-          >
-            <NavLink component={Link} to={'/fixtures/league/premier-league'} label='All Premier League Fixtures'/>
-
-            {PremierLeagueTeams.map((item) => (
-              <NavLink component={Link} to={`/fixtures/team/${item.url}`} key={item.url} label={item.display}/>
-            ))}
-
-
-          </NavLink>
-          </NavLink>
-          </Box>    
+          </Menu.Item>
+        </Menu.Dropdown>
+        </Menu>
+        
 
           <Menu withArrow offset={0} transitionProps={{transition: 'pop',duration:150}}>
             <Menu.Target>
