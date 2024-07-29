af842b99365eddffa6dcd8e6cd1bb5a91755cfa3
KyleNicholls19 <kyle.nicholls190@gmail.com>
2024-07-28 20:03:31 -0600

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -44,6 +44,66 @@ def get_table_data(page_data):
 
     return df 
 
+
+def get_result_data(page_data,isTeam):
+    '''
+    Searches the page and finds and store the fixture data into a data frame
+    team_fixtures is a boolean to tell if the search is for a league's fixtures or a specific team
+    team_fixtures = False if looking for a league's fixtures, True if looking for a team's fixtures
+    '''
+    # Finds all of the match dates and stores the text into a list
+    dates = page_data.find_all('h4')
+    dates_text = [date.text.strip() for date in dates]
+
+    # If looking for fixtures for a specific team
+    if (isTeam):
+        fixture_type = page_data.find_all('h5')
+        fixture_type_text = [fix.text.strip() for fix in fixture_type]
+
+
+    fix = page_data.find('div',{'class':'fixres__body'}) # Entire fixture list
+    matches_per_day = []
+    for i in range(len(dates)):
+        # Splits the fixtures by date and counts how many fixtures are on each date
+        if i < len(dates)-1:
+
+            matches_per_day.append(fix.prettify().split('<h4 class="fixres__header2">')[i+1].count('<div class="fixres__item">'))
+        else:
+            matches_per_day.append(fix.prettify().split('<h4 class="fixres__header2">')[i+1].split('<script data-role="load-more-content" type="text/show-more">')[0].count('<div class="fixres__item">'))
+
+    # Create the data frame to store the data
+    if not isTeam:
+        column_names = ['Match Date','Team 1','Team 2','Time']
+    else:
+        column_names = ['Match Date','Team 1','Team 2','Time','Fixture Type']
+    df = pd.DataFrame(columns = column_names)
+
+    # Finds the data for all the matches
+    match_info = page_data.find_all(class_ ='matches__item matches__link')
+    match_index_counter = 0 # counter to correlate which match relates to the position in match_info
+
+
+    # Loops through every date and for every date find the information of the matches on that date
+    # Uses the amount of matches per day and the match_index_counter to correlate which matches take place on which days
+    for i in range(len(dates)):
+        for j in range(matches_per_day[i]):
+            # Finds the match currently being iterated on per day
+            target_match = match_info[match_index_counter].find_all('span',{'class':'swap-text__target'})[:2]
+            teams = [team.text.strip() for team in target_match]
+
+            # Finds the match time for the current match
+            match_time = match_info[match_index_counter].find('span',{'class':'matches__date'})
+            time = match_time.text.strip()
+
+            if not isTeam:
+                df.loc[len(df)] = [dates_text[i],teams[0],teams[1],time] # enters the data into the data frame
+            else:
+                df.loc[len(df)] = [dates_text[i],teams[0],teams[1],time,fixture_type_text[match_index_counter+1]] # enters the data into the data frame
+
+            match_index_counter += 1
+    return(df)
+
+
 def get_fixture_data(page_data, team_fixtures):
     '''
     Searches the page and finds and store the fixture data into a data frame
@@ -132,6 +192,21 @@ def run_fixture_data(league,name):
     page_data = BeautifulSoup(page.text, 'html.parser')
     data = get_fixture_data(page_data, team_fixtures)
     return data.to_json(orient='index')
+
+
+@app.route('/results/<league>/<name>')
+def run_result_data(league,name):
+    url =f'https://www.skysports.com/{name}-results'
+    isTeam = True
+    if league == 'league':
+        isTeam = False
+
+
+    page = requests.get(url)
+    page_data = BeautifulSoup(page.text, 'html.parser')
+    data = get_fixture_data(page_data, isTeam)
+    return data.to_json(orient='index')
+
 
 
 def main():
