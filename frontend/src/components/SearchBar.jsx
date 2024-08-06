f1f92282e0c6d15b36af63985ef73b7eb6effd0c
KyleNicholls19 <kyle.nicholls190@gmail.com>
2024-08-05 20:49:32 -0600

    Update SearchBar.jsx

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,15 +1,28 @@
-import { Autocomplete, rem } from "@mantine/core"
+import { Autocomplete, rem } from "@mantine/core";
 import { IconSearch } from '@tabler/icons-react';
+import data from '../NameConversion.json';
+import { useState } from "react";
 
 
 function SearchBar() {
+
+    const [leagueNames,setLeagueNames] = useState([])
+
+    const leagueSearch = data.names.map((item) =>{
+        setLeagueNames([...l,item])
+    })
+
+
 
 
     return(
         <Autocomplete
         placeholder="Search"
         leftSection={<IconSearch style={{ width: rem(16), height: rem(16) }} stroke={1.5} />}
-        data={['React', 'Angular', 'Vue', 'Next.js', 'Riot.js', 'Svelte', 'Blitz.js']}
+        data={[
+            { group: 'Leagues', items: [leagueNames]}
+        ]}
+        limit={5}
         />
     )
 }
