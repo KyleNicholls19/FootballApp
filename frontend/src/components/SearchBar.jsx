6bf08656a68e45955c570840fd97dd5c6361a1b8
KyleNicholls19 <kyle.nicholls190@gmail.com>
2024-08-06 20:19:27 -0600

    Update SearchBar.jsx

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,29 +1,43 @@
 import { Autocomplete, rem, Select } from "@mantine/core";
 import { IconSearch } from '@tabler/icons-react';
-import {names} from '../NameConversion.json';
+import dataset from '../NameConversion.json';
 import { useState } from "react";
 import { redirect, useNavigate } from "react-router-dom";
 
 
 function SearchBar() {
 
-    const leagueNames = names.map(item => item.display)
+    const data = []
+
+    dataset.names.forEach(league => {
+        data.push(league.display)
+        
+        const leagueKey = league.url;
+        if (dataset[leagueKey]) {
+          dataset[leagueKey].forEach(team => {
+            data.push(team.display)
+          });
+        }
+      });
+
+
     const navigate = useNavigate()
 
     function handleSubmit(value){
         if (value){
-            const name = names.find((item) => item.display === value)
+            //const name = names.find((item) => item.display === value)
 
-            navigate(`/fixtures/league/${name.url}`, {replace: true})
+            //navigate(`/fixtures/league/${name.url}`, {replace: true})
         }
 
     }
 
     return(
         <Select
+        label='Search'
         placeholder="Search"
         leftSection={<IconSearch style={{ width: rem(16), height: rem(16) }} stroke={1.5} />}
-        data={leagueNames}
+        data={data}
         limit={5}
         onChange={(value) => handleSubmit(value)}
         searchable
