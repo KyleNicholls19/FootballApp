4911050997927664e6c99d2f7843f1645009b3e2
KyleNicholls19 <kyle.nicholls190@gmail.com>
2024-08-09 22:36:45 -0600

    ui

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -50,7 +50,6 @@ function SearchBar() {
         placeholder="Search"
         leftSection={<IconSearch style={{ width: rem(16), height: rem(16) }} stroke={1.5} />}
         data={data}
-        limit={5}
         onChange={(value) => handleSubmit(value)}
         searchable
         clearable
